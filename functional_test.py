7d10649105e307fd7b2f79b4bc9d64be71434cb6
Olav Vahtras <vahtras@kth.se>
2014-12-18 18:22:18 +0100

    Basic view returns minimal HTML

--- functional_test.py
+++ functional_test.py
@@ -13,7 +13,6 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
         self.assertIn('To-Do', self.browser.title)
-        self.fail('not implmented')
 
     #Invitation to enter a to-do item
 
