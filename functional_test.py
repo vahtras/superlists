a0b4e6caae6b6e1273a497e3295944e88157c2b3
Olav Vahtras <vahtras@kth.se>
2014-12-19 22:53:04 +0100

    Functional test now checks we can input a to-do item

--- functional_test.py
+++ functional_test.py
@@ -11,17 +11,38 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
+        # Edith has heard about a cool new online to-do app. She goes
+        # to check out its homepage
         self.browser.get('http://localhost:8000')
+
+        # She ntoices the page title and heaer mention to-do lists
         self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_name('h1').text
+        self.assertIn('To-Do', header_text)
 
-    #Invitation to enter a to-do item
+        # She is invited to enter a to-do item straight away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputboxx.get_attribute('placeholder'),
+            'Enter a to-do item'
+            )
 
 
-    #Enter "Buy peacock feathers" into a text box
+        # She types "Buy peacock feathers" into a text box
+        inputbox.send_keys('Buy peacock feathers')
 
-    #On enter the page updates "1: Buy peacock feathers"
+        # When she hits enter the page updates, and the page lists
+        #  "1: Buy peacock feathers" as an item in a to-do list table
+        inputbox.send_keys(Keys.ENTER)
+        table = self.browser.find_element_by_id("id_list_table")
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacodk feathers' for row in rows)
+            )
 
-    #There is a textbox. Enter "Make a fly"
+        # There is still a textbox inviting her to add another item. She
+        # enters "Use peacock feathers to make a fly"
+        self.fail('Finish the test')
 
     #The page updates, both items are shown
 
