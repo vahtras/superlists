0e0f5439bfe91b21868cd6318b091475cbad4ce4
Olav Vahtras <vahtras@kth.se>
2014-12-25 19:37:18 +0100

    Polish diffs with reference repo

--- lists/tests.py
+++ lists/tests.py
@@ -1,7 +1,7 @@
 from django.core.urlresolvers import resolve
-from django.test import TestCase
 from django.http import HttpRequest
 from django.template.loader import render_to_string
+from django.test import TestCase
 
 from lists.views import home_page
 
@@ -11,8 +11,9 @@ class HomePageTest(TestCase):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
 
+
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-        expected_html  = render_to_string('home.html')
+        expected_html = render_to_string('home.html')
         self.assertEqual(response.content.decode(), expected_html)
--- superlists/urls.py
+++ superlists/urls.py
@@ -6,5 +6,5 @@ urlpatterns = patterns('',
     url(r'^$', 'lists.views.home_page', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    #url(r'^admin/', include(admin.site.urls)),
+    # url(r'^admin/', include(admin.site.urls)),
 )
