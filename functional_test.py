7746bfae47aa8f4dd343a0204c43a46fb73c739b
Olav Vahtras <vahtras@kth.se>
2014-12-25 15:46:29 +0100

    Front page HTML now generated from a template

--- functional_test.py
+++ functional_test.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -23,7 +24,7 @@ class NewVisitorTest(unittest.TestCase):
         # She is invited to enter a to-do item straight away
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
-            inputboxx.get_attribute('placeholder'),
+            inputbox.get_attribute('placeholder'),
             'Enter a to-do item'
             )
 
@@ -37,7 +38,8 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element_by_id("id_list_table")
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text == '1: Buy peacodk feathers' for row in rows)
+            any(row.text == '1: Buy peacock feathers' for row in rows),
+            "New to-do item did not appear in table"
             )
 
         # There is still a textbox inviting her to add another item. She
