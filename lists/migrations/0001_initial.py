60b80cc9873a7762583adb452ad5f6f16d23c230
Olav Vahtras <vahtras@kth.se>
2014-12-30 01:14:21 +0100

    Polish diff with ref

--- lists/migrations/0001_initial.py
+++ lists/migrations/0001_initial.py
@@ -14,6 +14,7 @@ class Migration(migrations.Migration):
             name='Item',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+
             ],
             options={
             },
--- superlists/urls.py
+++ superlists/urls.py
@@ -2,7 +2,6 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    # Examples:
     url(r'^$', 'lists.views.home_page', name='home'),
     url(r'^lists/', include('lists.urls')),
     # url(r'^admin/', include(admin.site.urls)),
